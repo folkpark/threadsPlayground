d360b6924529b01a6f7d47563cbf8dffa66884e3
pfolkman <parkerfolkman@gmail.com>
2019-11-11 21:52:18 -0700

    Generating ATT data now

--- procScheduling/sched.c
+++ procScheduling/sched.c
@@ -18,9 +18,9 @@ gcc -std=c99 sched.c -o sched -lpthread -lgsl -lgslcblas -lm
 //Global Variables
 int algType = 0;
 int guassian_nums[10000];
-int num_of_threads =  10;
+int num_of_threads =  200;
 int k = 1000;
-//int execTimes_srt[num_of_threads];
+double d;
 
 typedef struct node{ //Used to implement a Queue of procs
 		int id;
@@ -32,29 +32,62 @@ typedef struct node{ //Used to implement a Queue of procs
 typedef TAILQ_HEAD(head_s, node) head_t;
 
 void *fifo_spn_Scheduler(head_t * head){
+	
+	FILE *outfile;
+	
+	if(algType==1){
+		outfile = fopen("FCFS_Data.csv","a");
+	}else{
+		outfile = fopen("SPN_Data.csv","a");
+	}
+	
+	int TT;
+	int TT_arr[num_of_threads];
 	struct node *e  = NULL;
 	clock_t cpuStart, cpuEnd, procStop, procStart;
 	cpuStart = clock();
-	cpuEnd = clock() + k; //Window of availability
+	cpuEnd = cpuStart + k; //Window of availability
 	int remaingCPU;
+	int counter = 0;
 	
 	while(!TAILQ_EMPTY(head) && clock() < cpuEnd){
 		e = TAILQ_FIRST(head);
-		procStart = clock();
+		procStart = clock() - cpuStart;
+		while(procStart < e->arrivalTime){ //Wait until the next proc is ready
+			procStart = clock() - cpuStart;
+		}
+		//printf("procStart = %ld\n", procStart);
 		procStop = procStart + e->execTime;
 		while(clock() < procStop){
 			//Process performs its action here
 		}
 		remaingCPU = cpuEnd - clock();
-		printf("Proc_ID: %d, Proc_Arrive: %d, Proc_ExecTime: %d, Remaing_Time: %d\n", 
-		e->id, e->arrivalTime,e->execTime, remaingCPU);
+		TT = procStop - e->arrivalTime; //Calc turn around time
+/* 		printf("Proc_ID: %d, Proc_Arrive: %d, Proc_ExecTime: %d, Remaing_Time: %d, TT: %d\n", 
+		e->id, e->arrivalTime,e->execTime, remaingCPU,TT); */
+		TT_arr[counter] = TT;
+		counter +=1;
 		TAILQ_REMOVE(head,e,nodes);
 		free(e);
 		e = NULL;
 	}
+	int TTsum = 0;
+	for(int i=0;i<num_of_threads;i++){
+		TTsum += TT_arr[i];
+	}
+	double ATT = TTsum/num_of_threads;
+	double d_ratio = d/ATT;
+	fprintf(outfile,"%d,%f\n",(int)d,d_ratio);
+	fclose(outfile);
+	ATT = 0;
+	d_ratio = 0;
+	d = 0;
 }
 
 void *srtScheduler(head_t * head){
+	FILE *outfile;
+	outfile = fopen("SRT_Data.csv","w");
+	
 	struct node *e  = NULL;
 	clock_t cpuStart, cpuEnd, procStop, procStart;
 	cpuStart = clock();
@@ -106,7 +139,6 @@ void printQueue(head_t * head){
 			printf("Thread_ID: %d",e->id);
 			printf(" arrivalTime: %d",e->arrivalTime);
 			printf(" ExecTime: %d\n",e->execTime);
-			
 	}
 }
 
@@ -135,9 +167,9 @@ int selectRandGauss(){ //Selects random int from gaussian distribution
 	return (guassian_nums[index]);
 }
 
-int selectRand(){ //Used for execution time
+int selectRand(int offset){ //Used for execution time
 	//Generates rand numbers from 1-50
-	return(rand() % 50 + 1);
+	return(rand() % 50 + 1 +offset);
 }
 
 void printNodes(struct node *procDetails, int x){
@@ -148,60 +180,70 @@ void printNodes(struct node *procDetails, int x){
 	}
 }
 
+void calc_d(struct node *procDetails){
+	int sum;
+	for(int i=0;i<num_of_threads;i++){
+		sum += procDetails[i].execTime;
+	}
+	d = sum/num_of_threads;
+}
+
 int main(int argc, char** argv){
 	algType = atoi(argv[1]); //Type of algorithm being tested
 	
 	srand( time(NULL) );
 	buildGaussian();
-	
-	/* 1. Give the process an arrival time based on gaussian distribution
-	   2. Give the process a random execTime
-	   3. put these into a list of nodes*/
-	struct node procDetails[num_of_threads];
-	for(int i=0;i<num_of_threads;i++){
-		struct node e;
-		e.id = i;
-		e.arrivalTime = selectRandGauss();
-		e.execTime = selectRand();
-		procDetails[i] = e;
-	}
-	
-	head_t head; //Head of the queue
-	TAILQ_INIT(&head);
-	pthread_t procs[num_of_threads]; // List of threads
-	
-	switch( algType ){
-		case 1: //FIFO
-			printf("Evaluating FCFS... \n");
-			//Sort the process details based on arrival time
-			qsort(procDetails,num_of_threads,sizeof(struct node),compareArrivalTime);
-			//printNodes(procDetails,num_of_threads);
-			buildQueue(&head, procDetails);
-			//printQueue(&head);
-			fifo_spn_Scheduler(&head);
-			break;
-			
-		case 2: //SPN
-			printf("Evaluating SPN... \n");
-			//Sort the process details based on arrival time
-			qsort(procDetails,num_of_threads,sizeof(struct node),compareExecTime);
-			buildQueue(&head, procDetails);
-			printQueue(&head);
-			fifo_spn_Scheduler(&head);
-			break;
-			
-		case 3: //SRT
-			qsort(procDetails,num_of_threads,sizeof(struct node),compareExecTime);
-			buildQueue(&head, procDetails);
-			printQueue(&head);
-			srtScheduler(&head);
-			break;
-			
-		default:
-			printf("Not a valid algType entry \n"); 
+	int trials = 25*3;
+	for(int offset=0;offset<trials;offset+=3){
+			/* 1. Give the process an arrival time based on gaussian distribution
+		   2. Give the process a random execTime
+		   3. put these into a list of nodes*/
+		struct node procDetails[num_of_threads];
+		for(int i=0;i<num_of_threads;i++){
+			struct node e;
+			e.id = i;
+			e.arrivalTime = selectRandGauss();
+			e.execTime = selectRand(offset);
+			procDetails[i] = e;
+		}
+		
+		head_t head; //Head of the queue
+		TAILQ_INIT(&head);
+		pthread_t procs[num_of_threads]; // List of threads
+		calc_d(procDetails);
+		
+		switch( algType ){
+			case 1: //FIFO
+				printf("Evaluating FCFS... \n");
+				//Sort the process details based on arrival time
+				qsort(procDetails,num_of_threads,sizeof(struct node),compareArrivalTime);
+				//printNodes(procDetails,num_of_threads);
+				buildQueue(&head, procDetails);
+				//printQueue(&head);
+				fifo_spn_Scheduler(&head);
+				break;
+				
+			case 2: //SPN
+				printf("Evaluating SPN... \n");
+				//Sort the process details based on arrival time
+				qsort(procDetails,num_of_threads,sizeof(struct node),compareExecTime);
+				buildQueue(&head, procDetails);
+				printQueue(&head);
+				fifo_spn_Scheduler(&head);
+				break;
+				
+			case 3: //SRT
+				qsort(procDetails,num_of_threads,sizeof(struct node),compareExecTime);
+				buildQueue(&head, procDetails);
+				printQueue(&head);
+				srtScheduler(&head);
+				break;
+				
+			default:
+				printf("Not a valid algType entry \n"); 
+		}
 	}
 
-	
     pthread_exit(NULL);
     return 0;
 }
